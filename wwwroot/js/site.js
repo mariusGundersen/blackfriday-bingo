d422a0a7f22d8780d4328629eab526b7904809ea
Marius Gundersen <marius.gundersen@clave.no>
2018-11-22 21:39:33 +0100

    Logos should be links

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,10 +1,4 @@
-﻿for(const item of document.querySelectorAll(".item")){
-  item.addEventListener('click', e =>{
-    item.toggleAttribute()
-  }, false);
-}
-
-setInterval(async () => {
+﻿setInterval(async () => {
   var victims = await fetch('/api/victims').then(r => r.json());
   for(const victim of victims){
     const item = document.querySelector(`.item[data-id="${victim.id}"] div`);
