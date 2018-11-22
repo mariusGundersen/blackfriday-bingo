3c58e6e2117e53b9e604f0b7f1eb2decc337b773
Marius Gundersen <marius.gundersen@clave.no>
2018-11-22 20:32:50 +0100

    Show a timeline of when the sites are up or down

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -10,6 +10,14 @@ setInterval(async () => {
     const item = document.querySelector(`.item[data-id="${victim.id}"] div`);
     if(item) {
       item.dataset["isAlive"] = victim.isAlive ? "True" : "False";
+      const history = item.querySelector('.history');
+      history.innerHTML = '';
+      for(const status of victim.history){
+        const elm = document.createElement('div');
+        elm.className = "status";
+        elm.dataset["status"] = status ? "True" : "False";
+        history.appendChild(elm);
+      }
     }
   }
 }, 1000*30);
